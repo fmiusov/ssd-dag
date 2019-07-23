e19e0b0f84556e2103ff3f5c8d9a54ed2023d2a8
Jay Duff <jay.duff@cfacorp.com>
2019-07-23 20:45:46 +0000

    model config

--- code/move_missing_pb2.sh
+++ code/move_missing_pb2.sh
@@ -3,8 +3,7 @@
 # extract the tarball of missing software and copy the *_pb2.py files
 # to projects/models/reearch/object_detection/protos/
 
-MODEL_RESEARCH="${HOME}/projects/models/research"
-MISSING_TARGET_PATH="${MODEL_RESEARCH}/object_detection/protos/"
+MISSING_TARGET_PATH="object_detection/protos/"
 
 tar -xvf missing_pb2.tar.gz
 mv missing_pb2/*_pb2.py ${MISSING_TARGET_PATH}
