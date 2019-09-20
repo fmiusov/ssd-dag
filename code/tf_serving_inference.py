fed9b0897faba96ca1579d4d78bf8a3103a3b7eb
Jay Duff <jay.duff@cfacorp.com>
2019-09-20 09:56:56 -0400

    infer test

--- code/tf_serving_inference.py
+++ code/tf_serving_inference.py
@@ -7,23 +7,54 @@ TensorFlow Serving
 """
 
 import base64
+import os
+import sys
+import requests
+import tensorflow as tf
 
 # without this PATH append
 # it won't find nets - in the /slim directory
 cwd = os.getcwd()
 tf_serving_repo = os.path.expanduser('~/projects/serving')
-models = os.path.join(cwd, 'models/research/')
-slim = os.path.join(cwd, 'models/research/slim')
-sys.path.append(models)
-sys.path.append(slim)
-import requests
+sys.path.append(tf_serving_repo)
+
 
 # GLOBALS
-TEST_IMAGE = '/home'
+SERVER_URL = SERVER_URL = 'http://localhost:8501/v1/models/cfa_prod:predic'
+SAMPLE_IMAGE = '/home/jay.duff/projects/ssd-dag/data/jpeg_images/20190710_variety_1562781001.jpg'
+
 
 def main():
-    print ("hello world")
+    print ("-- TensorFlow Serving - MobileNet Inference --")
 
+    # read in the sample image & shape the numpy array
+    img = tf.keras.preprocessing.image.load_img(SAMPLE_IMAGE, target_size=[300, 300])   # PIL image
+    x = tf.keras.preprocessing.image.img_to_array(img)  # numpy array (300,300,3)
+    x32 = tf.keras.applications.mobilenet.preprocess_input(x[tf.newaxis,...])  # numpy (1,300,300,3) float32
+    print (type(x32), x32.shape)
+
+    # encode the array
+      # Compose a JSON Predict request (send JPEG image in base64).
+    jpeg_bytes = base64.b64encode(x32)
+    jpeg_string = jpeg_bytes.decode('utf-8')
+    print ("jpeg_bytes:", type(jpeg_bytes))
+    print (jpeg_bytes[:50])
+    print ("jpeg_striing:", type(jpeg_string))
+    print (jpeg_string[:50])
+
+    predict_request = '{"instances" : [{"b64": "%s"}]}' % jpeg_string
+
+      # Send few actual requests and report average latency.
+    total_time = 0
+    num_requests = 10
+    for _ in range(num_requests):
+        response = requests.post(SERVER_URL, data=predict_request)
+        response.raise_for_status()
+        total_time += response.elapsed.total_seconds()
+        prediction = response.json()['predictions'][0]
+
+    print('Prediction class: {}, avg latency: {} ms'.format(
+        prediction['classes'], (total_time*1000)/num_requests))
 
 if __name__ == '__main__':
   main()
